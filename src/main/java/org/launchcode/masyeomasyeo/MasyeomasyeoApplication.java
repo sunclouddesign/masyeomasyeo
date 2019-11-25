6db706c1b33f6bda98ea36a6ebb8015c57465995
sunclouddesign <sunclouddesign@gmail.com>
2019-11-25 11:39:24 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/masyeomasyeo/MasyeomasyeoApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.masyeomasyeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasyeomasyeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasyeomasyeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/masyeomasyeo/MasyeomasyeoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.masyeomasyeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasyeomasyeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
